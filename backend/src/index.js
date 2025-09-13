80536ecbc7ae663a403d9dc1f9e49f30e3393b1a
shivakumar454 <shivaku4554@gmail.com>
2025-09-13 14:41:37 +0530

    fix: updated cors and api url for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,15 +14,18 @@ dotenv.config();
 
 
 const PORT =process.env.PORT;
-const _dirname=path.resolve();
+const __dirname=path.resolve();
 
 app.use(cookieParser());
-app.use(cors(
-    {
-        origin:"http://localhost:5173",
-        credentials:true
-    }
-))
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173", 
+    "https://realtime-chatapp-frontend-pnn9.onrender.com"
+  ],
+  credentials: true,
+}));
+
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
@@ -32,7 +35,7 @@ app.use("/api/messages",messageRoutes);
 
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(_dirname,"/frontend/dist")));
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
     
 
     app.get("*",(req,res)=>{
